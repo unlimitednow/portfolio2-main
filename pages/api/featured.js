c9e42675b812515951346490c6570d15f6c022ee
Unlimited Now <113130752+unlimitednow@users.noreply.github.com>
2023-05-16 02:07:19 -0400

    Update featured.js

--- pages/api/featured.js
+++ pages/api/featured.js
@@ -1,32 +1,24 @@
 export default function handler(req, res) {
     // fetch your data here
     const content = {
-		"project": "My Supply Co.",
-		"url": "https://mysupplyco.com",
-		"repo": "Private",
-		"descriptionTitle": "D2C & B2B ecommerce site and blog",
-		"description": "with elegant solutions for a complex codebase and customer journey.",
-		"imageOptions": [
-		],
-		"images": [
-			{ "key": "mock-stack",	"hover": "right", 	"h": "1200", "w": "556", "url": "/img/msc-mock_stack/03.png" },
-			{ "key": "mock-stack",	"hover": "right",	"h": "1200", "w": "556", "url": "/img/msc-mock_stack/02.png" },
-			{ "key": "mock-stack",	"hover": "left",	"h": "1200", "w": "556", "url": "/img/msc-mock_stack/01.png" }
-		],
-		"stack": [
-			{ "key": "php", 		"name": "PHP", 			"type": "devicon" },
-			{ "key": "mysql", 		"name": "mySQL", 		"type": "devicon" },
-			{ "key": "javascript", 	"name": "JavaScript", 	"type": "devicon" },
-			{ "key": "jquery", 		"name": "jQuery", 		"type": "devicon" },
-			{ "key": "woocommerce", "name": "WooCommerce", 	"type": "devicon" },
-			{ "key": "wordpress",	"name": "Wordpress", 	"type": "devicon" },
-			{ "key": "html5",		"name": "HTML5", 		"type": "devicon" },
-			{ "key": "css3",		"name": "CSS3", 		"type": "devicon" },
-			{ "key": "sass",		"name": "SCSS", 		"type": "devicon" },
-			{ "key": "git",			"name": "Git(Hub)", 	"type": "devicon" },
-			{ "key": "google",		"name": "GA4/GTM/EEC", 	"type": "devicon" }
-		]
-	};
+      project: "My Featured Project",
+      url: "https://example.com",
+      repo: "my-repo",
+      descriptionTitle: "My Project Description",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      stack: ["React", "Next.js", "Framer Motion"],
+      imageOptions: {},
+      images: [
+        {
+          key: "image1",
+          url: "https://res.cloudinary.com/unlimitednow/image/upload/v1674960871/samples/417f24e5-9e72-41ce-a4dd-f5d004b3c996.png",
+          hover: "left",
+          h: 300,
+          w: 400,
+        },
+        
+      ],
+    };
   
     // return the data as JSON
     res.status(200).json({ content });
