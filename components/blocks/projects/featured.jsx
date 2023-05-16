d7c5982bec59356180e1bc15fd2a5b916803d161
Unlimited Now <113130752+unlimitednow@users.noreply.github.com>
2023-05-16 01:17:13 -0400

    Update featured.jsx

--- components/blocks/projects/featured.jsx
+++ components/blocks/projects/featured.jsx
@@ -1,41 +1,162 @@
-import FeaturedProject from '../../blocks/projects/featured2'
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { m, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
+import Badges from "../../utils/badge.list.util";
+import Icon from "../../utils/icon.util";
 
-// Section structure
-import Section 		from '../../structure/section';
-import Container 	from '../../structure/container';
-import Badges 		from '../../utils/badge.list.util'
-import Icon 		from '../../utils/icon.util'
-import SectionTitle from '../../blocks/section.title.block'
+import css from "../../../styles/sections/projects/featured.module.scss";
 
-import css 			from '../../../styles/sections/projects/featured.module.scss'
-import content 		from '../../../content/projects/featured.json'
+export default function FeaturedProject() {
+  const [content, setContent] = useState(null);
+  const controls = useAnimation();
+  const { ref, inView } = useInView({
+    threshold: 0.25,
+    triggerOnce: false,
+  });
 
-export default function FeaturedProjects() {
+  useEffect(() => {
+    async function fetchContent() {
+      const res = await fetch("/api/featured");
+      const { content } = await res.json();
+      setContent(content);
+    }
+    fetchContent();
+  }, []);
 
-	return (
-		<Section classProp={css.hasBg}>	
-			<Container spacing={'verticalXXXXLrg'}>
-				<SectionTitle
-					title="Featured Projects"
-					preTitle="UX and Full Stack"
-					subTitle="Focused on the experience, driven by the engineering."
-				/> 				{
-				content.map( (data, index) => {
-					return (
-						<FeaturedProject content={data} index={index} key={index} />
-					)
-				})
-				}
-			</Container>
-			<div className={css.bgContainer}>
-				<span className={css.orbitalBg}>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroLeft} ${css.heroOrbital}`}></span></span>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroCenter}`}></span></span>
-					<span class={`${css.bgSection}`}><span className={`${css.bgInner} ${css.heroRight} ${css.heroOrbital}`}></span></span>
-				</span>
-				<span className={css.afterGlowBg}></span>
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  if (!content) {
+    return null;
+  }
+
+  const { project, url, repo, descriptionTitle, description, stack, imageOptions, images } = content;
+
+  return (
+    <m.section
+      className={css.project}
+      //framer-motion
+      ref={ref}
+      variants={container}
+      initial={["rest", "hidden"]}
+      whileHover="hover"
+      animate={controls}
+    >
+      <div className={css.details}>
+        <div className={css.projectHeader}>
+          <div className={css.header}>
+            <h3 className="highlight">
+              {project}
+            </h3>
+            <span className={css.privateOr}>
+              <i className="devicon-github-plain"></i>
+              {repo}
+            </span>
+          </div>
+          <div className={css.description}>
+            <p>
+              <strong>{descriptionTitle}</strong> {description}
+            </p>
+          </div>
+
+					<div className={css.stackContainer}>
+						<Badges list={stack} block="stack" fullContainer={false} color={false} />
+					</div>
+					<m.div variants={''} className={css.viewProject}>
+						<Icon icon={[ 'fad', 'arrow-right-to-bracket' ]} />
+					</m.div>
+				</div>
 			</div>
-		</Section>
+
+			<div className={css.imageContainer}>
+				<span className={`${css.imageAnimationContainer}`}>
+					{ images.map( ({key, url, hover, h, w }, index) => {
+						hover = ( hover === 'left' ) ? hoverLeft : hoverRight
+						return (
+							<m.div key={`${index}-${key}`} variants={item}>
+								<m.div variants={hover}>
+									<Image src={url} alt="x" height={h} width={w} />
+								</m.div>
+							</m.div>
+						)}
+					) }
+				</span>
+			</div>
+		</m.section>
 	)
 }
+
+const container = {
+	hidden: { 
+		transition: {
+			delayChildren: 0.125,
+			staggerChildren: 0.0625
+		}
+	},
+	visible: {
+		transition: {
+			delayChildren: 0.125,
+			staggerChildren: 0.25,
+		}
+	},
+	rest: {
+		transition: {
+			delayChildren: 0,
+			staggerChildren: 0,
+		}
+	},
+	hover: {
+		transition: {
+			delayChildren: 0,
+			staggerChildren: 0,
+		}
+	}
+}
+
+const item = {
+	hidden: { 
+		y: 75, 
+		opacity: 0,
+		transition: {
+			type: "tween",
+			ease: "easeIn",
+			duration: .35, 
+		}
+	},
+	visible: {
+		y: 0,
+		opacity: 1,
+		transition: {
+			type: "tween",
+			ease: "easeOut",
+			duration: .5, 
+		}
+	},
+}
+
+const hoverLeft = {
+	rest: {
+		x: 0
+	},
+	hover: {
+		x: -20
+	}
+}
+
+const hoverRight = {
+	rest: {
+		x: 0
+	},
+	hover: {
+		x: 20
+	}
+}
+
