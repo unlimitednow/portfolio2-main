d3876516825ef30d4856fc07f388daed4d57820a
Unlimited Now <113130752+unlimitednow@users.noreply.github.com>
2023-05-16 01:38:13 -0400

    Update featured.js

--- pages/api/featured.js
+++ pages/api/featured.js
@@ -24,7 +24,30 @@ export default function handler(req, res) {
           w: 400,
         },
       ],
-    };
+    }, {
+      project: "My Featured Project",
+		"url": "https://github.com/atlamors/portfolio",
+		"repo": "Public",
+		"descriptionTitle": "An open source portfolio",
+		"description": "built on Next.js and React. A fast and agile MERN stack single page application.",
+		"imageOptions": [
+			{ "key": "size", 		"value": "large" }
+		],
+		"images": [
+			{ "key": "portfolio", "hover": "left", "h": "797", "w": "556",  "url": "/img/portfolio-mock_single.png" }
+		],
+		"stack": [
+			{ "key": "nextjs", 		"name": "Next.js", 		"type": "devicon" },
+			{ "key": "react", 		"name": "React", 		"type": "devicon" },
+			{ "key": "nodejs", 		"name": "Node.js", 		"type": "devicon" },
+			{ "key": "mongodb", 	"name": "MongoDB", 		"type": "devicon" },
+			{ "key": "javascript", 	"name": "JavaScript", 	"type": "devicon" },
+			{ "key": "html5",		"name": "HTML5", 		"type": "devicon" },
+			{ "key": "css3",		"name": "CSS3", 		"type": "devicon" },
+			{ "key": "sass",		"name": "SCSS", 		"type": "devicon" },
+			{ "key": "git",			"name": "Git(Hub)", 	"type": "devicon" }
+		]
+	};
   
     // return the data as JSON
     res.status(200).json({ content });
