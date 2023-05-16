b8c717add5ce97b0c82a4427adbe69250e345639
Unlimited Now <113130752+unlimitednow@users.noreply.github.com>
2023-05-16 03:18:05 -0400

    Update featured.js

--- pages/api/featured.js
+++ pages/api/featured.js
@@ -1,43 +1,32 @@
 export default function handler(req, res) {
-  // fetch your data here
-  const content = [
-    {
-      "project": "My Supply Co.",
-      "url": "https://mysupplyco.com",
-      "repo": "Private",
-      "descriptionTitle": "D2C & B2B ecommerce site and blog",
-      "description": "with elegant solutions for a complex codebase and customer journey.",
-      "imageOptions": [],
-      "images": [
-        { "key": "mock-stack", "hover": "right", "h": "1200", "w": "556", "url": "/img/msc-mock_stack/03.png" },
-        { "key": "mock-stack", "hover": "right", "h": "1200", "w": "556", "url": "/img/msc-mock_stack/02.png" },
-        { "key": "mock-stack", "hover": "left", "h": "1200", "w": "556", "url": "/img/msc-mock_stack/01.png" }
+    // fetch your data here
+    const content = {
+      project: "My Featured Project",
+      url: "https://example.com",
+      repo: "my-repo",
+      descriptionTitle: "My Project Description",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      stack: ["React", "Next.js", "Framer Motion"],
+      imageOptions: {},
+      images: [
+        {
+          key: "image1",
+          url: "/image1.jpg",
+          hover: "left",
+          h: 300,
+          w: 400,
+        },
+        {
+          key: "image2",
+          url: "/image2.jpg",
+          hover: "right",
+          h: 300,
+          w: 400,
+        },
       ],
-      "stack": [
-        { "key": "php", "name": "PHP", "type": "devicon" },
-        { "key": "mysql", "name": "mySQL", "type": "devicon" },
-        { "key": "google", "name": "GA4/GTM/EEC", "type": "devicon" }
-      ]
-    },
-    {
-      "project": "andrewnelson.net",
-      "url": "https://github.com/atlamors/portfolio",
-      "repo": "Public",
-      "descriptionTitle": "An open source portfolio",
-      "description": "built on Next.js and React. A fast and agile MERN stack single page application.",
-      "imageOptions": [
-        { "key": "size", "value": "large" }
-      ],
-      "images": [
-        { "key": "portfolio", "hover": "left", "h": "797", "w": "556", "url": "/img/portfolio-mock_single.png" }
-      ],
-      "stack": [
-        { "key": "nextjs", "name": "Next.js", "type": "devicon" },
-        { "key": "git", "name": "Git(Hub)", "type": "devicon" }
-      ]
-    }
-  ];
-
-  // return the data as JSON
-  res.status(200).json({ content });
-}
+    };
+  
+    // return the data as JSON
+    res.status(200).json({ content });
+  }
+  
