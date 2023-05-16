6dcbf03d219b1eda3e91e4672cac809e243ebdf3
Unlimited Now <113130752+unlimitednow@users.noreply.github.com>
2023-05-16 01:45:25 -0400

    Update featured.js

--- pages/api/featured.js
+++ pages/api/featured.js
@@ -2,30 +2,6 @@ export default function handler(req, res) {
     // fetch your data here
     const content = {
       project: "My Featured Project",
-      url: "https://example.com",
-      repo: "my-repo",
-      descriptionTitle: "My Project Description",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      stack: ["React", "Next.js", "Framer Motion"],
-      imageOptions: {},
-      images: [
-        {
-          key: "image1",
-          url: "https://res.cloudinary.com/unlimitednow/image/upload/v1674960871/samples/417f24e5-9e72-41ce-a4dd-f5d004b3c996.png",
-          hover: "left",
-          h: 300,
-          w: 400,
-        },
-        {
-          key: "image2",
-          url: "public/img/msc-mock_stack/01.png",
-          hover: "right",
-          h: 300,
-          w: 400,
-        },
-      ],
-    }, {
-      project: "My Featured Project",
 		"url": "https://github.com/atlamors/portfolio",
 		"repo": "Public",
 		"descriptionTitle": "An open source portfolio",
